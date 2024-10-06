aa8d945a2202108e72ec5447d986d1a972d61e1d
Thibaut <thibautdumont@pm.me>
2024-10-06 12:22:58 +0200

    bis

--- app/api/script/[variable]/route.ts
+++ app/api/script/[variable]/route.ts
@@ -2,12 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 // Next.js will invalidate the cache when a
 // request comes in, at most once every 60 seconds.
-export const revalidate = 10; // seconds
+export const revalidate = 30; // seconds
 
 // We'll prerender only the params from `generateStaticParams` at build time.
 // If a request comes in for a path that hasn't been generated,
 // Next.js will server-render the page on-demand.
-export const dynamicParams = true; // or false, to 404 on unknown paths
+// export const dynamicParams = true; // or false, to 404 on unknown paths
+export const dynamic = "auto";
 
 // export async function generateStaticParams() {
 //   const posts: Post[] = await fetch('https://api.vercel.app/blog').then((res) =>
