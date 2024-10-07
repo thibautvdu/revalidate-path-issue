e9be4b8d312eec477f844184af232cde3044545b
Thibaut <thibautdumont@pm.me>
2024-10-07 07:49:29 +0200

    script

--- app/api/script/[variable]/route.ts
+++ app/api/script/[variable]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 // Next.js will invalidate the cache when a
 // request comes in, at most once every 60 seconds.
-export const revalidate = 60; // seconds
+// export const revalidate = 60; // seconds
 
 // We'll prerender only the params from `generateStaticParams` at build time.
 // If a request comes in for a path that hasn't been generated,
