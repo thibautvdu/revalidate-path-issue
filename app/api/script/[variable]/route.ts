e9a2379a304f753ec059ac3ab6947ef84dfacbe7
Thibaut <thibautdumont@pm.me>
2024-10-07 16:51:27 +0200

    going back to regular cache

--- app/api/script/[variable]/route.ts
+++ app/api/script/[variable]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force-static"; // Forces static generation
-export const revalidate = 3600; // Revalidate every hour (3600 seconds)
+// export const dynamic = "force-static"; // Forces static generation
+// export const revalidate = 3600; // Revalidate every hour (3600 seconds)
 
 export async function GET(
   request: NextRequest,
@@ -10,11 +10,11 @@ export async function GET(
   console.log("Regenerating script with variable", params.params.variable);
 
   return new NextResponse(
-    `hello variable ${params.params.variable} ; date: ${Date.now()}`
-    // {
-    //   headers: {
-    //     "Vercel-CDN-Cache-Control": "max-age=86400", // 1 day
-    //   },
-    // }
+    `hello variable ${params.params.variable} ; date: ${Date.now()}`,
+    {
+      headers: {
+        "Vercel-CDN-Cache-Control": "max-age=86400", // 1 day
+      },
+    }
   );
 }
